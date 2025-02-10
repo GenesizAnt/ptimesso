c0d12ae4c4403449d08307effb217f0c4da57a70
Anton <genesiz1990@gmail.com>
2025-02-10 22:00:27 +0300

    Initial commit

---
+++ src/main/java/ptimesso/PtimessoApplication.java
@@ -0,0 +1,13 @@
+package ptimesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtimessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtimessoApplication.class, args);
+	}
+
+}
